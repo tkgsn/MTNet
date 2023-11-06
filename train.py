a476b9c2aee613eec528ebec2982595db8e02a1e
tkgsn <shun.0721@hotmail.co.jp>
2023-11-06 20:46:58 +0000

    send param

--- train.py
+++ train.py
@@ -116,6 +116,8 @@ if __name__ == '__main__':
     training_settings = {"dataset": data_dir.split('/')[1], "data_name": data_dir.split('/')[-2], "network_type": "MTNet"}
     with open(config.SAVE_DIR / 'training_settings.json', 'w') as f:
         json.dump(training_settings, f)
+    
+    send(config.SAVE_DIR / 'training_settings.json')
     config.PARAM_BASE = config.SAVE_DIR
     n_data = 0
     with open(config.DATA_DIR / 'training_data.csv', 'r') as f:
